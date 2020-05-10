7fcac8ed1e38583c69ce95132cf3486ceecda755
Jogesh-Mishra <64301279+Jogesh-Mishra@users.noreply.github.com>
2020-05-10 21:18:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/task_1/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.task_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private View mProgressView;
+    private View mLoginFormView;
+    private TextView tvLoad;
+
+
+    Button btnSignout;
+    TextView tvMessage,tvCall,tvBrowser,tvFacebook,tvInstagram;
+
+    FirebaseAuth fauth ;
+    FirebaseUser fireUser ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+        tvLoad = findViewById(R.id.tvLoad);
+
+        btnSignout=findViewById(R.id.btnSignout);
+        tvMessage=findViewById(R.id.tvMessage);
+        tvCall=findViewById(R.id.tvCall);
+        tvBrowser=findViewById(R.id.tvBrowser);
+        tvFacebook=findViewById(R.id.tvFacebook);
+        tvInstagram=findViewById(R.id.tvInstagram);
+
+        fauth = FirebaseAuth.getInstance();
+        fireUser = fauth.getCurrentUser();
+
+
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProgress(true);
+                tvLoad.setText("Signing Out. Please Wait !");
+
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(MainActivity.this,LogIn.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "User Successfullly Logged Out", Toast.LENGTH_SHORT).show();
+                showProgress(false);
+            }
+        });
+
+        tvCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:"+"7008683249";
+               Intent intent =new Intent(Intent.ACTION_DIAL);
+               intent.setData(Uri.parse(uri));
+               startActivity(intent);
+            }
+        });
+
+        tvMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setData(Uri.parse("sms:"));
+                startActivity(sendIntent);
+            }
+        });
+
+        tvBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "https://"+"www.spectrumcet.com";
+                Intent web = new Intent(Intent.ACTION_VIEW);
+                web.setData(Uri.parse(uri));
+                startActivity(web);
+            }
+        });
+
+        tvFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fb = new Intent(Intent.ACTION_VIEW);
+                String uri ="https://"+"facebook.com" ;
+                fb.setData(Uri.parse(uri));
+                startActivity(fb);
+            }
+        });
+
+        tvInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent insta = new Intent(Intent.ACTION_VIEW);
+                String uri = "https://instagram.com/";
+                insta.setData(Uri.parse(uri));
+                startActivity(insta);
+            }
+        });
+    }
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+
+            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoad.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+}
